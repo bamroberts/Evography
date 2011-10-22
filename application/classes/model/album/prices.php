5b4444c167d46d7bb866a4d881ddc169c50b5acd
Ben Roberts <iam@bamroberts.com>
2011-10-22 16:05:33 +0100

    Album price model added

---
+++ application/classes/model/album/prices.php
@@ -0,0 +1,11 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Model_Album_Prices extends ORM {
+    
+    protected $_table_name='album_prices';
+    protected $_primary_key='album_id';
+    protected $_belongs_to=array(
+      'album'=>array('model'=>'album','foreign_key'=>'id'),
+    );   
+}
+  
