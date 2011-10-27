5bc96a45155a7cb48b56d01dfafaa9a4a286d93a
Ben Roberts <iam@bamroberts.com>
2011-10-27 23:50:07 +0100

    reworking of admin menus

---
+++ application/classes/controller/admin/menu.php
@@ -0,0 +1,10 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Admin_Menu extends Master_Admin {
+
+  function action_index(){
+    $items = Orm::factory('menu',$this->request->param('id',1))->children;
+    $this->content->template=View::factory('admin/block/menu')
+      ->bind('items',$items);
+    }
+}
