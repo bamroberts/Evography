8ba4ec8d6d22895fe4d3541a89bee6cac0a85010
Ben Roberts <iam@bamroberts.com>
2011-11-04 01:02:32 +0000

    Nicely structured inheriting image upload class

--- application/classes/controller/admin/upload.php
+++ application/classes/controller/admin/upload.php
@@ -1,113 +1,217 @@
 <?php 
-  class Controller_Admin_Upload extends Controller_AdminTemplate {
+class Controller_Admin_Upload extends Controller_Admin_Album_core {
+    protected $source='internal';
+    protected $max_size=array('width'=>2048,'height'=>2048);
+    protected $keep_original = false;
   	
   	function action_index(){
-  	  $this->template->content = View::factory('pages/admin/upload')
-      ->bind('results', $results)
-      ->bind('page_links', $page_links);
+  	  $this->template->content = View::factory('admin/album/upload')
+      ->bind('facebook', $facebook);
+      
+      $facebook = Request::factory($this->request->url(array('controller'=>'uploadFacebook')))->execute();
+      
+      if ($_FILES) $this->import();
+      
   	}
   	
-  	function action_save(){  
-  	if (!$_FILES) return;
-  	$files = new Validation($_FILES);
-            $files->rules(
-                'Filedata',
-                array(
-                    array('Upload::not_empty', NULL),
-                    array('Upload::valid' , NULL),
-                    array('Upload::size', array(':value','20M')),
-                    array('Upload::type' , array(
-                        ':value',
-                        array('jpg', 'png', 'gif', 'jpeg')
-                    )),
-                )
-            );	
-   if($files->check()) {
-         //if passed checks save imag in new home 
-            $user_id=Auth::instance()->get_user();
-            $album_id=$this->request->param('id');
-            $album_id=1;
-			$path = DOCROOT."images/uploads/{$user_id}/{$album_id}/";
-			if (!is_dir($path)) {
-			  mkdir($path,0777,true);
-			}
-			
-			$file=pathinfo($_FILES['Filedata']['name']);	
-			$filename     =$file['basename'];
-			$name		  =$file['filename'];	
-			$ext          =strtolower($file['extension']);
-			
-			$hash         =uniqid();
-			
-			$new_filename ="{$hash}.{$ext}";
-			
-			$filepath=Upload::save($_FILES['Filedata'], $new_filename, $path, 0777);
-		    $size         =filesize($filepath);
-			list($width,$height) = @getimagesize($filepath);
-		//STORE IMAGE IN DB	
-		//Get filename and path parts	
-		 
-		 //Get exif photodata
-		  $meta=exif_read_data($filepath);
-			  $camdata["Make"]        =Arr::get($meta, 'Make');
-			  $camdata["Model"]       =Arr::get($meta, 'Model');
-			  $camdata["Aperture"]    =Arr::get(Arr::get($meta, 'COMPUTED'), 'ApertureFNumber');
-			  $camdata["Exposure"]    =Arr::get($meta, 'ExposureTime');
-			  $camdata["ISO"]         =Arr::get($meta, 'ISOSpeedRatings');
-			  $camdata["Orientation"] =Arr::get($meta, 'Orientation')==1?'Landscape':'Portrait';
-			  $camdata["Lens"]  	  =Arr::get($meta, 'UndefinedTag:0x0095');
-			  $camdata["Focal Length"]=Arr::get(Arr::get($meta, 'UndefinedTag:0x0002'), 1);
-		  $record["meta"]=serialize($camdata);
-		 
-		  $record["filehash"] =$hash;
-		  $record["filename"] =$filename;
-		  $record["path"]     =str_replace('\\','/',str_replace(DOCROOT,'',$filepath)); 
+  	function import($return_file=false){
+  	   $files = new Validation($_FILES);
+       $files->rules('Filedata',array(   
+          array('Upload::not_empty', NULL),
+          array('Upload::valid' , NULL),
+          array('Upload::size', array(':value','20M')),
+          array('Upload::type' , array(
+                  ':value', array('jpg', 'png', 'gif', 'jpeg')
+                  ),
+               ),
+            )
+        );	
+    
+        $album_id = $this->request->param('id');
+        $user_id  = Auth::instance()->get_user();  
+        
+        $targetDir=     DOCROOT."images/uploads/{$user_id}/{$album_id}/";
+        
+        $chunk = isset($_REQUEST["chunk"]) ? $_REQUEST["chunk"] : 0;
+        $chunks = isset($_REQUEST["chunks"]) ? $_REQUEST["chunks"] : 0;
+        $fileName = isset($_REQUEST["name"]) ? $_REQUEST["name"] : $_FILES['file']['name'];
+    
+        $fileName = preg_replace('/[^\w\._]+/', '', $fileName);
+        
+        // Make sure the fileName is unique but only if chunking is disabled
+        if ($chunks < 2 && file_exists($targetDir . DIRECTORY_SEPARATOR . $fileName)) {
+        	$ext = strrpos($fileName, '.');
+        	$fileName_a = substr($fileName, 0, $ext);
+        	$fileName_b = substr($fileName, $ext);
+        
+        	$count = 1;
+        	while (file_exists($targetDir . DIRECTORY_SEPARATOR . $fileName_a . '_' . $count . $fileName_b))
+        		$count++;
+        
+        	$fileName = $fileName_a . '_' . $count . $fileName_b;
+        }
+        
+        // Create target dir
+        if (!file_exists($targetDir))
+        	@mkdir($targetDir);
+        	
+        // Look for the content type header
+        if (isset($_SERVER["HTTP_CONTENT_TYPE"]))
+        	$contentType = $_SERVER["HTTP_CONTENT_TYPE"];
+        
+        if (isset($_SERVER["CONTENT_TYPE"]))
+        	$contentType = $_SERVER["CONTENT_TYPE"];
+        
+        // Handle non multipart uploads older WebKit versions didn't support multipart in HTML5
+        if (strpos($contentType, "multipart") !== false) {
+        	if (isset($_FILES['file']['tmp_name']) && is_uploaded_file($_FILES['file']['tmp_name'])) {
+        		// Open temp file
+        		$out = fopen($targetDir . DIRECTORY_SEPARATOR . $fileName, $chunk == 0 ? "wb" : "ab");
+        		if ($out) {
+        			// Read binary input stream and append it to temp file
+        			$in = fopen($_FILES['file']['tmp_name'], "rb");
+        
+        			if ($in) {
+        				while ($buff = fread($in, 4096))
+        					fwrite($out, $buff);
+        			} else
+        				die('{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}');
+        			fclose($in);
+        			fclose($out);
+        			@unlink($_FILES['file']['tmp_name']);
+        		} else
+        			die('{"jsonrpc" : "2.0", "error" : {"code": 102, "message": "Failed to open output stream."}, "id" : "id"}');
+        	} else
+        		die('{"jsonrpc" : "2.0", "error" : {"code": 103, "message": "Failed to move uploaded file."}, "id" : "id"}');
+        } else {
+        	// Open temp file
+        	$out = fopen($targetDir . DIRECTORY_SEPARATOR . $fileName, $chunk == 0 ? "wb" : "ab");
+        	if ($out) {
+        		// Read binary input stream and append it to temp file
+        		$in = fopen("php://input", "rb");
+        
+        		if ($in) {
+        			while ($buff = fread($in, 4096))
+        				fwrite($out, $buff);
+        		} else
+        			die('{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}');
+        
+        		fclose($in);
+        		fclose($out);
+        	} else
+        		die('{"jsonrpc" : "2.0", "error" : {"code": 102, "message": "Failed to open output stream."}, "id" : "id"}');
+        }
+        
+        $filepath=$targetDir . DIRECTORY_SEPARATOR . $fileName;
+        
+        $image=$this->save($filepath);
+        if ($return_file) return $image;
+        
+        
+        if ($this->request->param('format')=='json') {
+            $return["jsonrpc"] = "2.0";
+            $return["result"] = null;
+            $return["id"] = "id";
+            $return["status"] = "1";
+            $this->template->content=json_encode($return + $image->as_array() );
+            return;
+      } else {
+        Hint::set(Hint::SUCCESS,"Your image was successfully uploaded.");
+		    $this->request->redirect($this->request->initial()->uri(array('action'=>'')));
+    }  
 
-		  $record["width"]    =$width;
-		  $record["height"]   =$height;
-		  
-		  $record["size"]     =$size;
-		  
-		  $record["ext"]      =$ext;
-		  $record["taken"]    =date('Y-m-d H:i:s',time(Arr::get($meta, 'DateTime')));
-		  $record["name"] 	  =$name;
-		  $record["active"]   =1;
-		  
-		  $record["add_user_id"]= $user_id;
-		  $record["mod_user_id"]= $user_id;
-		  $record["add_date"]= date('Y-m-d H:i:s');
-		  $record["mod_date"]= date('Y-m-d H:i:s');
-		  $record["album_id"]= $album_id;
-		  $record['order']= ORM::factory('album',$album_id)->images->order_by('order','DESC')->find()->order + 1;
-		  
-		  $image=ORM::factory('image');
-	      $image->values($record);
-	      $image->check();
-	      $image->save();
-	      $image->add('albums', $album_id);
 
-		  $return=$image->as_array();	
-		  $return['status']=1;
-		  
-   } else {	 
-		    $return = array(
-			 'status' => '0',
-			 'error' => "Upload and server side validation failed"
-			);
-   }
-	
-   if ($this->request->is_ajax() || Request::$user_agent=='Adobe Flash Player 10') {
-      	    $this->auto_render = FALSE;
-            $this->is_ajax = TRUE;
-            header('content-type: application/json');
-            $this->response->body( json_encode($return) );
-    }
-     
-    $this->template->body="<pre>".print_r($return,true)."</pre>"; 
-      
-  }
-}
+  	}
   	
-  	
+  	function save($filepath){
   	 
-  
+  	 $file=pathinfo($filepath);
+  	 list($width,$height) = @getimagesize($filepath);
+  	 
+  	 //lets compress the file
+  	 $image = Image::factory($filepath);
+  	 //and resize is massive;
+  	 if ($width>$max_width=Arr::get($this->max_size,'width',10000) AND $height>$max_height=Arr::get($this->max_size,'height',10000)){
+  	   $image->resize($max_width,$max_height,Image::AUTO);
+  	   //new with and height
+  	   $width=$image->width;
+		   $height=$image->height;
+  	 }
+  	 //do we keep the original as a backup
+  	 if ($this->keep_original) {$filepath=$filepath."_r";}
+  	 //save a compressed copy
+  	 $image->save($filepath);
+  	 
+  	 
+  	 $meta=exif_read_data($filepath);
+  	 $cam_data=$this->cam_data($meta);
+  	   
+  	 $album = $this->node;
+  	 $user_id  = Auth::instance()->get_user();
+  	 
+  	 $image=ORM::factory('image');
+  	 $image->set('album_id'   , $album->id                        )
+  	       ->set('add_user_id', $user_id                          )
+  	       ->set('filehash'   , uniqid()                          )
+  	       ->set('filename'   , $file['basename']                 )
+  	       ->set('name'       , $file['filename']                 )
+  	       ->set('ext'        , strtolower( $file['extension'] )  )
+  	       ->set('active'     , 1                                 )
+  	       ->set('source'     , $this->source                     )
+  	       ->set('add_date'   , date('Y-m-d H:i:s')               )
+  	       ->set('meta'       , $cam_data                         )
+  	       ->set('path'       , str_replace('\\','/',str_replace(DOCROOT,'',$filepath))  )
+  	       ->set('taken'      , date('Y-m-d H:i:s',time(  Arr::get($meta, 'DateTime') )) )
+  	       ->set('order'      , ORM::factory('album',$album_id)->images->count_all() + 1 );
+  	        
+     if (Arr::get($meta, 'Orientation',1)) 
+     {
+       $image->set('width',$width)
+             ->set('height',$height);
+     } 
+     
+     else 
+     {
+       $image->set('rotate',1)
+             ->set('width',$height)
+             ->set('height',$width);
+     }
+  	       
+     $image->check();
+     $image->save();
+
+     return $image;  	
+  	}
+  	
+    function cam_data($meta){
+  	  if ( method_exists($this,$make='data_'.strtolower(Arr::get($meta, 'Make'))) )
+  	  {
+  	   return serialize($this->$make($meta));
+      }
+  	}
+  	
+  	function data_samsung($meta){
+  	     $camdata["Make"]        = Arr::get($meta, 'Make');
+			   $camdata["Model"]       = Arr::get($meta, 'Model');
+			   $camdata["Aperture"]    = Arr::get( Arr::get($meta, 'COMPUTED') , 'ApertureFNumber');
+			   $camdata["Exposure"]    = Arr::get($meta, 'ExposureTime');
+			   $camdata["ISO"]         = Arr::get($meta, 'ISOSpeedRatings');
+			   $camdata["Orientation"] = Arr::get($meta, 'Orientation')==1?'Landscape':'Portrait';
+			   //user for debug
+			   $camdata["Raw"]         = $meta;
+			 return $camdata;
+		}
+  	
+  	function data_canon($meta){
+  	     $camdata["Make"]        = Arr::get($meta, 'Make');
+			   $camdata["Model"]       = Arr::get($meta, 'Model');
+			   $camdata["Aperture"]    = Arr::get( Arr::get($meta, 'COMPUTED') , 'ApertureFNumber');
+			   $camdata["Exposure"]    = Arr::get($meta, 'ExposureTime');
+			   $camdata["ISO"]         = Arr::get($meta, 'ISOSpeedRatings');
+			   $camdata["Orientation"] = Arr::get($meta, 'Orientation')==1?'Landscape':'Portrait';
+			   $camdata["Lens"]  	     = Arr::get($meta, 'UndefinedTag:0x0095');
+			   $camdata["Focal Length"]= Arr::get( Arr::get($meta, 'UndefinedTag:0x0002') , 1);
+			 return $camdata;
+  	}
+  	
+  }
---
+++ application/classes/controller/admin/uploadfacebook.php
@@ -0,0 +1,111 @@
+<?php 
+class Controller_Admin_UploadFacebook extends Controller_Admin_Upload {
+  protected $source='facebook';
+    
+  protected $_model = array(
+      'album'=>array(  
+          'name'=>'Select a FB album',
+          'formtype'=>'select',
+          'options'=>array(),
+        ), 
+      'all'=>array(
+          'name'=>'All images',
+          'formtype'=>'all',
+          'from'=>'images',
+        ), 
+      'images'=>array(  
+          'name'=>'Pick images',
+          'formtype'=>'image_select',
+          'options'=>array(),
+        ),
+  );
+    
+  public function before(){
+    parent::before();
+    if (! $this->facebook=Facebook::factory() )
+    {
+      $this->request->action('login');
+    }
+  }  
+    
+  public function action_albums(){
+    $this->template->content=$this->get_albums();
+  }
+  
+  public function action_images(){
+    $this->template->content=$this->get_album_images();
+  }
+  
+  public function action_index(){
+    $this->template->content = View::factory('admin/facebook/import')
+      ->set('user'  ,  $this->facebook->user()    )
+      ->set('albums',  $this->get_albums()        )
+      ->set('images',  $this->get_album_images()  );
+  }
+  
+  public function action_login(){
+    $this->template->content = Facebook::login();
+  }
+
+  public function get_albums(){
+    $columns=$this->_model;
+    $albums = $this->facebook->get('/me/albums?limit=0');
+    
+    foreach ($albums['data'] as $key=>$album) {
+      $columns['album']['options'][$album['id']]="{$album['name']} - ". Arr::get($album,'count',0)." images";
+    }
+  
+    $post=$this->request->initial()->post();
+    
+    return View::factory('admin/facebook/albums')
+      ->bind('columns',$columns)    
+      ->bind('data',$post)
+      ->bind('errors',$errors);
+  }
+  
+  public function get_album_images(){
+    $post=$this->request->initial()->post();
+    //If no album is selected return 
+    if ( ! $album_id = Arr::get($post,'album')) return;
+    
+    $columns=$this->_model; 
+           
+    $images = $this->facebook->get("/{$album_id}/photos/?fields=id,picture,images,from&limit=0");
+    
+    foreach ($images['data'] as $key=>$image) {
+      $columns['images']['options'][$key]=Arr::get($image,'picture',0);
+    }
+       
+    //import is set, import selected images
+    if (is_array($import=Arr::get($post,'images',false) ) ) 
+    {
+      $image=$images['data'];
+      unset($_POST['images']);
+      
+      $user_id=$this->user_id;
+      $album_id=$this->request->param('id');
+      $path = DOCROOT."images/uploads/{$user_id}/{$album_id}/";
+		  if (!is_dir($path)) {
+		   mkdir($path,0777,true);
+		  }
+      
+      foreach ($import as $id=>$tick) {
+        $remote=$image[$id]['images'][0]['source'];
+        $file=pathinfo($remote);	
+			  $new_filename = uniqid().'.'.strtolower($file['extension']);
+			  
+        $get=file_get_contents($remote);
+        $put=file_put_contents($path.$new_filename,$get);
+        $filepath=$path.$new_filename; 
+        $this->save($filepath);
+      }
+    }
+      
+    return View::factory('admin/facebook/images')
+          ->bind('album',$album_id  )
+          ->bind('columns',$columns)
+          ->bind('data',$post)
+          ->bind('errors',$errors)
+          ;
+    }
+}
