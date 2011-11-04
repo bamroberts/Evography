f053800fbaa6947e56f253a7ff1d850c592e62a1
Ben Roberts <iam@bamroberts.com>
2011-11-04 01:00:11 +0000

    Tidied this up

--- application/views/pages/admin/login.php
+++ application/views/pages/admin/login.php
@@ -1,8 +1,39 @@
 <form method="post">
-  <fieldset>
+
     <?php echo Helpers::render_form($columns,$data,$errors); ?>
-    <a class="block" href="<?php echo Request::current()->url(array('action'=>'reset_password')); ?>">Forgotten your password?</a>
-  </fieldset>
+    <hr />
+    
+    <div class="group text">
+     <label>Username or Email</label>
+     <div class="input">
+      <input type="text" name="username" value="" />
+     </div>
+    </div>
+    
+    <div class="group password">
+     <label>Password</label>
+     <div class="input">
+      <input type="Password" name="password" value="" />
+      <ul class="inputs-list">
+        <li>
+          <label>
+            <input type="checkbox" name="username" value="" />
+            <span>Keep me logged in</span>
+          </label>
+        </li>
+       </ul>
+     </div>
+    </div>  
+    
+    <div class="group">
+     <label></label>
+     <div class="input">
+       
+     </div>
+    </div> 
+  
+    <a class="pull-right" href="<?php echo Request::current()->url(array('action'=>'reset_password')); ?>">Forgotten your password?</a>    
+
   <div class="actions">
     <a type="submit" href="#">Cancel</a>
     <button class="btn default" type="submit">Login</button>
