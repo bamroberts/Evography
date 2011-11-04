9b6426aef35ce4fb0e6ba09e608dca7ea74596a3
Ben Roberts <iam@bamroberts.com>
2011-11-04 00:57:04 +0000

    Config file for Facebook app

---
+++ application/config/facebook.php
@@ -0,0 +1,7 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+return array(
+      'appId' => '179663692087801',
+      'secret' => 'edb719a604ac38a60a1900383e0edfec',
+      'cookie' => true,   
+      );
