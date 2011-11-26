61acb59e2000d9194de041bf8518fa243b53b42f
Ben Roberts <iam@bamroberts.com>
2011-11-26 13:07:46 +0000

    Invite model

--- application/classes/model/invite.php
+++ application/classes/model/invite.php
@@ -2,4 +2,30 @@
 class Model_Invite extends ORM
 {
 protected $_table_name = 'invite';
+
+	public function rules()
+	{
+		return array(
+			'name' => array(
+				array('not_empty'),
+				array('min_length', array(':value', 4)),
+			),
+			'email' => array(
+				array('not_empty'),
+				array('min_length', array(':value', 4)),
+				array('max_length', array(':value', 127)),
+				array('email'),
+				array(array($this, 'email_available'), array(':validation', ':field')),
+			),
+		);
+	}
+	
+	public function email_available(Validation $validation, $field)
+  {
+    if ($this->unique_key_exists($validation[$field], 'email'))
+    {
+        $validation->error($field, 'email_available', array($validation[$field]));
+    }
+  }
+	
 }
